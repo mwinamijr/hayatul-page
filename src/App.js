bfa91efb02b260858f15b900239da46a3e803189
mwinamijr <mwinamijr@gmail.com>
2023-10-12 16:38:02 +0300

    created basic screens for the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
 
 function App() {
   return (
     <Router>
       <Header />
-
+      <Routes to="/">
+        <Route index element={<HomeScreen />} />
+        <Route path='/about' element={<AboutScreen />} />
+        <Route path='contact-us' element={<ContactScreen />} />
+      </Routes>
       <Footer />
     </Router>
   );
